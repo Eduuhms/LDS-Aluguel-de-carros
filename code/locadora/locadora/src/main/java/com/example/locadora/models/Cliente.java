a883c412652ae00013b1baebaf857ac81a51a5cc
roberta sophia <robertasophiacsilva@gmail.com>
2025-03-27 11:23:06 -0300

    feat: base springboot para o cliete criada

---
+++ code/locadora/locadora/src/main/java/com/example/locadora/models/Cliente.java
@@ -0,0 +1,42 @@
+package com.example.locadora.models;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "clientes")
+public class Cliente {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String nome;
+    private String email;
+
+    // Getter e Setter para 'id'
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    // Getter e Setter para 'nome'
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    // Getter e Setter para 'email'
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
